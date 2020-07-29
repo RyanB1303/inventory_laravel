7b54583814a2b7a82eb03cdf35f46357ad5c36d6
Ryan B <ryan.brilliant.nirwana@gmail.com>
2020-07-29 15:13:08 +0700

    Revert "Dashboard experimental"

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,24 +2,14 @@ require("./bootstrap");
 
 import Vue from "vue";
 import VueRouter from "vue-router";
-import App from "./components/App.vue";
-import Navbar from "./components/Navbar.vue";
+
 Vue.use(VueRouter);
-import { routes } from "./routes.js";
-// Vue.component(
-//     "example-component",
-//     require("./components/ExampleComponent.vue").default
-// );
-const router = new VueRouter({
-    routes,
-    mode: "history" //change to history again to remove #
-});
+
+Vue.component(
+    "example-component",
+    require("./components/ExampleComponent.vue").default
+);
 
 const app = new Vue({
-    el: "#app",
-    components: {
-        App,
-        Navbar
-    },
-    router
+    el: "#app"
 });
--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/{vue_capture?}', function(){
-    return view('welcome');
-})->where('vue_capture', '[\/\w\.-]*');
-
 // Auth::routes();
 
 // Route::get('/home', 'HomeController@index')->name('home');
