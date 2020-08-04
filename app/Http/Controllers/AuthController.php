1e6a9e6199b1dc95b1217a875107636541b70dad
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2020-08-04 18:33:42 +0700

    adding custom message

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -38,7 +38,7 @@ class AuthController extends Controller
         $credentials = request(['email', 'password']);
 
         if (! $token = auth()->attempt($credentials)) {
-            return response()->json(['error' => 'Unauthorized'], 401);
+            return response()->json(['error' => 'Invalid Email or Password'], 401);
         }
 
         return $this->respondWithToken($token);
