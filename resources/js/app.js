7ec9b5a3193f34225564382564dfe558a483b266
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2020-08-04 19:26:36 +0700

    idk

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,13 @@ require("./bootstrap");
 
 import Vue from "vue";
 import VueRouter from "vue-router";
+<<<<<<< HEAD
 
+=======
+import App from "./components/App.vue";
+import Navbar from "./components/Navbar.vue";
+import Sidebar from "./components/Sidebar.vue";
+>>>>>>> 1e6a9e6199b1dc95b1217a875107636541b70dad
 Vue.use(VueRouter);
 
 Vue.component(
@@ -11,5 +17,15 @@ Vue.component(
 );
 
 const app = new Vue({
+<<<<<<< HEAD
     el: "#app"
+=======
+    el: "#app",
+    components: {
+        App,
+        Navbar,
+        Sidebar
+    },
+    router
+>>>>>>> 1e6a9e6199b1dc95b1217a875107636541b70dad
 });
--- resources/js/components/Sidebar.vue
+++ resources/js/components/Sidebar.vue
@@ -1,4 +1,5 @@
 <template>
+<<<<<<< HEAD
   <ul class="navbar-nav sidebar sidebar-light accordion" id="accordionSidebar">
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
       <div class="sidebar-brand-icon">
@@ -139,11 +140,119 @@
     <div class="version" id="version-ruangadmin"></div>
   </ul>
   <!-- Sidebar -->
+=======
+    <ul
+        class="navbar-nav sidebar sidebar-light accordion"
+        id="accordionSidebar"
+    >
+        <router-link
+            :to="{ name: 'home' }"
+            class="sidebar-brand d-flex align-items-center justify-content-center"
+        >
+            <div class="sidebar-brand-icon">
+                <img :src="'/backend/img/logo/logo2.png'" />
+            </div>
+            <div class="sidebar-brand-text mx-3">RuangAdmin</div>
+        </router-link>
+        <hr class="sidebar-divider my-0" />
+        <li class="nav-item active">
+            <router-link :to="{ name: 'dashboard' }" class="nav-link">
+                <i class="fas fa-fw fa-tachometer-alt"></i>
+                <span>Dashboard</span></router-link
+            >
+        </li>
+        <hr class="sidebar-divider" />
+        <div class="sidebar-heading">
+            Features
+        </div>
+        <li class="nav-item">
+            <a
+                class="nav-link collapsed"
+                href="#"
+                data-toggle="collapse"
+                data-target="#collapseForm"
+                aria-expanded="true"
+                aria-controls="collapseForm"
+            >
+                <i class="fab fa-fw fa-wpforms"></i>
+                <span>Forms</span>
+            </a>
+            <div
+                id="collapseForm"
+                class="collapse"
+                aria-labelledby="headingForm"
+                data-parent="#accordionSidebar"
+            >
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <h6 class="collapse-header">Forms</h6>
+                    <router-link
+                        :to="{ name: 'form-basic' }"
+                        class="collapse-item"
+                        >Form Basics</router-link
+                    >
+                </div>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a
+                class="nav-link collapsed"
+                href="#"
+                data-toggle="collapse"
+                data-target="#collapseTable"
+                aria-expanded="true"
+                aria-controls="collapseTable"
+            >
+                <i class="fas fa-fw fa-table"></i>
+                <span>Tables</span>
+            </a>
+            <div
+                id="collapseTable"
+                class="collapse"
+                aria-labelledby="headingTable"
+                data-parent="#accordionSidebar"
+            >
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <h6 class="collapse-header">Tables</h6>
+                    <router-link :to="{ name: 'table' }" class="collapse-item"
+                        >Simple Table</router-link
+                    >
+                    <router-link
+                        :to="{ name: 'data-table' }"
+                        class="collapse-item"
+                        >Data Table</router-link
+                    >
+                </div>
+            </div>
+        </li>
+        <hr class="sidebar-divider" />
+        <li class="nav-item">
+            <router-link :to="{ name: 'login' }" class="nav-link"
+                >Login</router-link
+            >
+            <router-link :to="{ name: 'register' }" class="nav-link"
+                >Register</router-link
+            >
+        </li>
+        <hr class="sidebar-divider" />
+        <li class="nav-item">
+            <router-link :to="{ name: 'charts' }" class="nav-link">
+                <i class="fas fa-fw fa-chart-area"></i>
+                <span>Charts</span>
+            </router-link>
+        </li>
+        <hr class="sidebar-divider" />
+        <div class="version" id="version-ruangadmin"></div>
+    </ul>
+>>>>>>> 1e6a9e6199b1dc95b1217a875107636541b70dad
 </template>
 
 <script>
 export default {};
 </script>
 
+<<<<<<< HEAD
 <style>
 </style>
+=======
+<style></style>
+>>>>>>> 1e6a9e6199b1dc95b1217a875107636541b70dad
