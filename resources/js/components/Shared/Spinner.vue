c2a777df16efe141f1016689645c2ad324015d5d
Ryan B <ryan.brilliant.nirwana@gmail.com>
2020-08-07 10:34:30 +0700

    adding login functionality and some spinner to animate login process

---
+++ resources/js/components/Shared/Spinner.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="overlay" v-if="show" @click.self="show = !show">
+        <fulfilling-bouncing-circle-spinner
+            :animation-duration="4000"
+            :size="60"
+            color="#ff1d5e"
+        />
+    </div>
+</template>
+
+<script>
+import { FulfillingBouncingCircleSpinner } from "epic-spinners";
+
+export default {
+    props: ["show"],
+    components: {
+        FulfillingBouncingCircleSpinner
+    },
+    data() {
+        return {
+            show: true
+        };
+    }
+};
+</script>
+
+<style scoped>
+.overlay {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #16244759;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    z-index: 200;
+}
+</style>
